09f1cb836fcf23dd626632c3c7fe651f396fbf17
rywata <168230259+rywata@users.noreply.github.com>
2026-04-13 20:22:55 -0300

    Update Pedidos-gui.py
    Higienização da chave

--- Pedidos-gui.py
+++ Pedidos-gui.py
@@ -39,9 +39,10 @@ if not check_password():
 # --- 1. CONFIGURAÇÃO DO GOOGLE SHEETS ---
 @st.cache_resource
 def conectar_google():
+    # 1. Carrega os dados
     info = dict(st.secrets["gcp_service_account"])
-    if "\\n" in info["private_key"]:
-        info["private_key"] = info["private_key"].replace("\\n", "\n")
+    raw_key = info["private_key"].strip()
+    info["private_key"] = raw_key.replace("\\n", "\n")
     
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     
