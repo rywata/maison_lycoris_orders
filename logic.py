eacd135403fd939b50d21b678ea6c836bbc9c21a
rywata <168230259+rywata@users.noreply.github.com>
2026-04-19 17:27:02 -0300

    Update logic.py
    refactor: implementa @property para garantir integridade dos cálculos

--- logic.py
+++ logic.py
@@ -1,24 +1,38 @@
 class Carrinho:
-    def __init__(self, lista_de_itens, nomes_dos_pasteis):
-        self.itens = lista_de_itens
-        self.pasteis = nomes_dos_pasteis
+    def __init__(self, lista_de_itens=None, nomes_dos_pasteis=None):
+        self.itens = lista_de_itens if lista_de_itens is not None else []
+        self.pasteis = nomes_dos_pasteis if nomes_dos_pasteis is not None else []
+
+    def adicionar_item(self, produto: str, qtd: int, preco_unitario: float):
+        if qtd <= 0:
+            raise ValueError("A quantidade deve ser maior que zero.")
+        #Gargalo de produção no shokupan
+        if produto == "Shokupan" and qtd > 20:
+            raise ValueError("Limite diário de Shokupan excedido")
+
+        novo_item = {
+            'produto': produto,
+            'qtd': qtd,
+            'preco_unitario': preco_unitario,
+            'subtotal': qtd * preco_unitario
+        }
+        self.itens.append(novo_item)
 
     @property
-    def total_bruto(self):
+    def total_bruto(self) -> float:
         return sum(item['subtotal'] for item in self.itens)
 
     @property
-    def tem_desconto(self):
+    def tem_desconto(self) -> bool:
         qtd_total_pasteis = sum(item['qtd'] for item in self.itens if item['produto'] in self.pasteis)
         return qtd_total_pasteis >= 4
 
     @property
-    def desconto_total(self):
+    def desconto_total(self) -> float:
         if not self.tem_desconto:
             return 0.0
         return sum(item['subtotal'] * 0.15 for item in self.itens if item['produto'] in self.pasteis)
 
     @property
-    def total_final(self):
+    def total_final(self) -> float:
         return self.total_bruto - self.desconto_total
-
