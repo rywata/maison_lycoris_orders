d48e7b65fa5beb232a9c71baa469b5b6465518c8
rywata <rafaelywata@gmail.com>
2026-05-10 00:30:16 -0300

    feat: adicionadas novas colunas no histórico de pedidos

--- historico_pedidos.py
+++ historico_pedidos.py
@@ -108,6 +108,7 @@ def renderizar_historico():
         'quantidade': 'Qtd',
         'total_bruto': 'Total Bruto',
         'custo_total': 'Curto Total',
+        'desconto': 'Desconto',
         'total_liquido': 'Total Líquido'
     }
 
