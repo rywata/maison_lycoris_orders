fe8de82221a50cc24bcb268b34cb350c71efd73c
rywata <rafaelywata@gmail.com>
2026-07-17 17:01:59 -0300

    Adicionados novos produtos

--- pedidos.py
+++ pedidos.py
@@ -14,9 +14,17 @@ def renderizar_novo_pedido():
         st.session_state.carrinho = []
 
     cardapio = {
-        "Pão de Leite": 15.0, "Pão Integral": 17.0, "Pão Semi Integral": 17.0, "Shokupan": 17.0,
-        "Pastel de Nata": 7.0, "Pastel de Maçã": 7.0, "Pastel de Ricota com Ervas Finas": 7.0,
-        "Pastel de Frango com Parmesão": 7.0, "Pastel de Amendoim": 7.0, "Croissant Amanteigado": 8.0
+        "Pão de Leite": 15.0,
+        "Pão Integral": 17.0,
+        "Pão Semi Integral": 17.0,
+        "Shokupan": 17.0,
+        "Pastel de Nata": 7.0,
+        "Pastel de Maçã": 7.0,
+        "Pastel de Ricota com Ervas Finas": 7.0,
+        "Pastel de Frango com Parmesão": 7.0,
+        "Pastel de Amendoim": 7.0,
+        "Croissant Amanteigado": 8.0,
+        "Brownie": 8.0
     }
     codigo_pasteis = [k for k in cardapio.keys() if "Pastel" in k]
 
